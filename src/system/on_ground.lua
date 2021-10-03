d5a4abb09bd41224d55f522a2ff3202dd2d2c166
IcyLava <icylavah@gmail.com>
2021-10-03 11:31:04 +0300

    Fix being able to jump on bombs

--- src/system/on_ground.lua
+++ src/system/on_ground.lua
@@ -1,9 +1,13 @@
 local filter = tiny.requireAll(util.bumpFilter, 'onGround')
 
+local function isGroundFilter(_, item)
+	return item.solid and 'slide'
+end
+
 local function process(self, e, dt)
 	local bump = self.world.bump
 	local ty = e.y + 0.1
-	local actualX, actualY, cols, len = bump:check(e, e.x, ty)
+	local actualX, actualY, cols, len = bump:check(e, e.x, ty, isGroundFilter)
 	local nog = actualY ~= ty
 	if nog ~= e.onGround then
 		e.onGround = nog
